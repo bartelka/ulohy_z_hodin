5df93074ad5b80574e7b4b6b613a5aeb081677a5
bartelka <113470730+bartelka@users.noreply.github.com>
2023-09-11 11:55:57 +0000

    dalsie

---
+++ 4.rocnik/abeceda.py
@@ -0,0 +1,17 @@
+from string import ascii_lowercase
+
+hlbka = 3
+vysledok = []
+for i in range(hlbka):
+    vysledok.append("a")
+
+
+def genhesla(h):
+    if h == -1:
+        print(vysledok)
+    else:
+        for pis in ascii_lowercase:
+            vysledok[h-1] = pis
+            genhesla(h-1)
+
+genhesla(hlbka)
